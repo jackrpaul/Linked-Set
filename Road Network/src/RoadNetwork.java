976ca1396c99ddbcdba40233687fbfeb7ffdbf3b
Jack <Jack@172.22.238.223>
2018-09-10 17:26:14 -0400

    created methods

---
+++ Road Network/src/RoadNetwork.java
@@ -0,0 +1,56 @@
+
+public class RoadNetwork extends java.lang.Object
+	{
+		public RoadNetwork()
+			{
+			
+			}
+		public RoadNetwork(int maxSize)
+			{
+				
+			}
+		public boolean addCity()
+			{
+			return true;
+			}
+		public boolean areConnected()
+			{
+			return true;
+			}
+		public boolean areDirectlyConnected()
+			{
+			return true;
+			}
+		public boolean connect()
+			{
+			return true;
+			}
+		public boolean containsCity()
+			{
+			return true;
+			}
+		public void disconnect()
+			{
+			
+			}
+		public String[] getCities()
+			{
+			return null;
+			}
+		public int getCityCount()
+			{
+			return 0;
+			}
+		public String[] getDirectlyConnectedCities()
+			{
+			return null;
+			}
+		public void removeCity()
+			{
+			
+			}
+		public String toString()
+			{
+			return "";
+			}
+	}
