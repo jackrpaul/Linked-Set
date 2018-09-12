1aa95e39f7866a6e1171d7e3a9ade46546bfb126
Jack <Jack@172.25.135.201>
2018-09-12 15:46:14 -0400

    added Javadoc comments in City class

--- Road Network/src/City.java
+++ Road Network/src/City.java
@@ -1,9 +1,21 @@
+/**
+ * This class creates a City object to be used further in the RoadNetwork class.
+ * @author Jack Paul
+ * @version 1.0
+ */
 
 public class City
 	{
 		private String name;
 		private City[] connectedCities;
 		private int networkSize, numOfConnectedCities;
+		
+		/**
+		 * This method creates a City object with a designated name, list of connected cities, maximum network size, and number of connected cities, which is set at 0.
+		 * @param n The name of the City.
+		 * @param c The list of Cities that this City is connected to.
+		 * @param ne The maximum size of the road network the City is being added to.
+		 */
 		public City(String n, City[] c, int ne)
 			{
 			name = n;
@@ -11,26 +23,47 @@ public class City
 			numOfConnectedCities = 0;
 			connectedCities = new City[networkSize];
 			}
+		
+		/**
+		 * Finds the name of the City.
+		 * @return the name of the City.
+		 */
 		public String getName()
 			{
 				return name;
 			}
+		
+		/**
+		 * Changes the name of the City to name.
+		 * @param name the new name of the City
+		 */
 		private void setName(String name)
 			{
 				this.name = name;
 			}
+		
+		/**
+		 * Finds the list of Cities this City is connected to.
+		 * @return the list of Cities this City is connected to.
+		 */
 		public City[] getConnectedCities()
 			{
 				return connectedCities;
 			}
-		private void setConnectedCities(City[] connectedCities)
-			{
-				this.connectedCities = connectedCities;
-			}
+		
+		/**
+		 * Determines how many Cities this City is connected to.
+		 * @return the integer number of Cities this City is connected to.
+		 */
 		public int getNumOfConnectedCities()
 			{
 				return numOfConnectedCities;
 			}
+		
+		/**
+		 * Changes the number of Cities this City is connected to into numOfConnectedCities
+		 * @param numOfConnectedCities the new integer number of Cities that this City is connected to.
+		 */
 		public void setNumOfConnectedCities(int numOfConnectedCities)
 			{
 				this.numOfConnectedCities = numOfConnectedCities;
